21cbe14ade3269aa149fee16accb241c62034a2e
Sarika Mohanraj <66568393+sarikamohan08@users.noreply.github.com>
2021-10-17 23:44:14 +0530

    stage 1 complete

--- src/get_data.py
+++ src/get_data.py
@@ -16,9 +16,9 @@ def get_data(config_path):
     # print(config)
     data_path = config["data_source"]["s3_source"]
     df = pd.read_csv(data_path, sep=",", encoding='utf-8')
-    print(df.head())
+    return df
 
-
+#checking lock file~nothing mich
 
 if __name__=="__main__":
     args = argparse.ArgumentParser()
